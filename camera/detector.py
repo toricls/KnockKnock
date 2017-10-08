9c01f20e48fb0f6233932846c48f011703d81834
Sander van de Graaf <mail@svdgraaf.nl>
2017-10-08 11:18:50 -0400

    detect faces

--- camera/detector.py
+++ camera/detector.py
@@ -11,26 +11,27 @@ with picamera.PiCamera() as camera:
     camera.resolution = (320, 240)
     camera.capture(stream, format='jpeg')
 
-# Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+while True:
+    # Convert the picture into a numpy array
+    buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 
-# Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
+    # Now creates an OpenCV image
+    image = cv2.imdecode(buff, 1)
 
-# Get the cascade file (with the generic faces)
-face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+    # Get the cascade file (with the generic faces)
+    face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
 
-# Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    # Convert to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-# Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    # Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
-print "Found "+str(len(faces))+" face(s)"
+    print "Found "+str(len(faces))+" face(s)"
 
-# Draw a rectangle around every found face
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+    # Draw a rectangle around every found face
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
 
-# Save the result image
-cv2.imwrite('./results/result.jpg', image)
+    # Save the result image
+    cv2.imwrite('./results/result.jpg', image)
