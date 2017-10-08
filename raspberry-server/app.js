ed11ad487e4e3569630cdac7733fd856a9176dec
Bogdan Polovko <bogdan.polovko@gmail.com>
2017-10-08 16:57:50 -0400

    Rolled back

--- raspberry-server/app.js
+++ raspberry-server/app.js
@@ -17,7 +17,7 @@ const options = {
   mode: 'text',
   pythonOptions: ['-u'],
   scriptPath: '../camera',
-  pythonPath: '../camera',
+  // pythonPath: '../camera',
   // args: ['value1', 'value2', 'value3']
 };
 
