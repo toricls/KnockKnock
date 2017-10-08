d48822f9b5fa8f78af2e2c860f0fa1cd5605f8f1
Bogdan Polovko <bogdan.polovko@gmail.com>
2017-10-08 14:01:32 -0400

    Server is runnning

--- raspberry-server/app.js
+++ raspberry-server/app.js
@@ -1,13 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 const http = require('http');
 const app = require('express')();
 const server = http.createServer();
 const volleyball = require('volleyball');
 const port = process.env.PORT || 5656;
+const morgan = require('morgan');
 
+const logger = morgan('combined');
 
 app.get('/', (req, res) => {
+  console.log('Test');
   res.send({ result: 'Success!' });
+});
+
+app.use(logger);
+
+app.use((req, res, next) => {
+  if (path.extname(req.path).length > 0) {
+    res.status(404).end();
+  } else {
+    next(null);
+  }
 });
 
 server.on('request', app);
