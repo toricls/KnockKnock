8c6a61e330ff6147d65f05e4fc29ae61098636d6
Sander van de Graaf <mail@svdgraaf.nl>
2017-10-08 16:17:23 -0400

    also blink

--- camera/wave.py
+++ camera/wave.py
@@ -5,15 +5,20 @@ GPIO.setup(26, GPIO.OUT)
 p = GPIO.PWM(26, 0.5)
 pwm = GPIO.PWM(26, 50)
 
+# led
+GPIO.setup(7, GPIO.OUT)
+
 pwm.start(5)
 
 max = 10
 i = 0
 while i < max:
-	pwm.ChangeDutyCycle(2)
-	time.sleep(0.5)
-	pwm.ChangeDutyCycle(6)
-	time.sleep(0.5)
-	i += 1
+    pwm.ChangeDutyCycle(2)
+    GPIO.output(7, 1)
+    time.sleep(0.5)
+    pwm.ChangeDutyCycle(6)
+    GPIO.output(7, 0)
+    time.sleep(0.5)
+    i += 1
 
 GPIO.cleanup()
