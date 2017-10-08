7d3605eb8a94a6e906d3bfd9168038dbd852e789
toricls <toriclstr@gmail.com>
2017-10-08 12:58:23 -0400

    Fix a bug

--- functions/upload-lambda/index.js
+++ functions/upload-lambda/index.js
@@ -1,12 +1,11 @@
 const ENV = process.env.ENV;
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const queryString = require('query-string');
 
 // Expecting `name` and `type` will be provided as request body paramters of API call's
 module.exports = (event, context, callback) => {
   console.log(event)
-  let params = queryString.parse(event.body);
+  let params = JSON.parse(event.body);
   let s3Params = {
     Bucket: process.env.BUCKET_NAME,
     Key: params.name,
