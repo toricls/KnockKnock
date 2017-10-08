b0a232e228dfaaa82075de6a1f17deed6377eaa8
Bogdan Polovko <bogdan.polovko@gmail.com>
2017-10-08 14:40:08 -0400

    Added script to trigger waving

--- raspberry-server/app.js
+++ raspberry-server/app.js
@@ -3,14 +3,35 @@ const path = require('path');
 const http = require('http');
 const app = require('express')();
 const server = http.createServer();
-const volleyball = require('volleyball');
 const port = process.env.PORT || 5656;
 const morgan = require('morgan');
-
 const logger = morgan('combined');
 
-app.get('/', (req, res) => {
-  console.log('Test');
+const PythonShell = require('python-shell');
+
+const options = {
+  mode: 'text',
+  pythonOptions: ['-u'],
+  scriptPath: '../camera',
+  // args: ['value1', 'value2', 'value3']
+};
+
+app.get('/wave', (req, res) => {
+  PythonShell.run('wave.py', options, function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution 
+
+    res.send({ result: 'Success!' });
+  });
+});
+
+app.get('/sound', (req, res) => {
+  
+  res.send({ result: 'Success!' });
+});
+
+app.get('/light', (req, res) => {
+  
   res.send({ result: 'Success!' });
 });
 
